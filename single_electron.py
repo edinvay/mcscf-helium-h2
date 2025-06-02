de1eeda3edafd140b9a0ef5c574a49dead31b8e9
edinvay <evgeni36@yandex.ru>
2025-06-02 17:41:22 +0200

    introduced orthonormalised Hermite functions

--- single_electron.py
+++ single_electron.py
@@ -12,7 +12,8 @@ print(" ")
 
 
 
-from input import precision
+#from input import precision
+precision = 1.0e-3
 from input import ZERO
 
 
@@ -81,3 +82,43 @@ def get_multi_indices(N_orbitals):
 
 
 print(get_multi_indices(N_orbitals))
+
+
+
+
+def hermite_3d_eval(n, x, y, z):
+    """
+    Evaluate the 3D Hermite polynomial H_{n_x}(x) * H_{n_y}(y) * H_{n_z}(z)
+    at points (x, y, z).
+    
+    Inputs:
+        n : tuple (n_x, n_y, n_z) of nonnegative integers
+        x, y, z : floats or numpy arrays (must be broadcastable)
+    
+    Returns:
+        H_n(x, y, z) = H_{n_x}(x) * H_{n_y}(y) * H_{n_z}(z)
+    """
+    nx, ny, nz = n
+    return scipy.special.eval_hermite(nx, x) * scipy.special.eval_hermite(ny, y) * scipy.special.eval_hermite(nz, z)
+
+
+
+print(mra)
+P_mra = vp3.ScalingProjector(mra, precision)
+
+
+Guess_orbital = []
+
+for ind, multi_index in enumerate(get_multi_indices(N_orbitals)):
+    print(ind)
+    print(multi_index)
+    def f(x):
+        return np.exp( (- x[0]*x[0] - x[1]*x[1] - x[2]*x[2]) * 0.5 ) * hermite_3d_eval(multi_index, x[0], x[1], x[2])
+    print("projecting...")
+    guess = P_mra(f)
+    guess.normalize()
+    print(guess)
+    Guess_orbital.append(guess)
+
+Guess_orbital = np.array(Guess_orbital)
+print(np.linalg.norm(calculate_overlap(Guess_orbital, Guess_orbital) - np.eye(N_orbitals)))
