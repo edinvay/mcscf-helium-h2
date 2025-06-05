df21245b04fdca29c23f6ee8d4a83f944f4ba55c
edinvay <evgeni36@yandex.ru>
2025-06-05 15:17:35 +0200

    renaming single electron calculations to the initial guess

--- rename_guess_orbitals.sh
+++ rename_guess_orbitals.sh
@@ -12,8 +12,8 @@ fi
 
 # Perform the renaming
 for ((k=0; k<N_orbitals; k++)); do
-    src="experiments/${molecule_name}/guess_orbital_${k}.tree"
-    dst="experiments/${molecule_name}/guess_orbital_${k}(hydrogen_type).tree"
+    src="experiments/${molecule_name}/single_electron_${N_orbitals}_orbital_phi_${k}.tree" #single_electron_2_orbital_phi_0.tree
+    dst="experiments/${molecule_name}/guess_orbital_${k}.tree"
 
     if [[ -f "$src" ]]; then
         cp "$src" "$dst"
