4d36fe2f62cc8b5be7cc598b38df3003ea26176e
edinvay <evgeni36@yandex.ru>
2025-06-14 17:19:34 +0200

    restored eigh-method (symmetric matrices oriented) based lowdin

--- src/lowdin.py
+++ src/lowdin.py
@@ -3,7 +3,12 @@ import numpy as np
 from input import molecule_state
 
 from scipy.optimize import minimize
-from scipy.linalg import fractional_matrix_power
+
+
+def fractional_matrix_power(A, p):
+    sigma, U = np.linalg.eigh(A)
+    Sm5 = U @ np.diag(sigma**p) @ U.T
+    return Sm5
 
 
 def lowdin_orthonormalization(Phi, coeff):
