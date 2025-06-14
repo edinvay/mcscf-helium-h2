f33f8d1c089391aea851941cafc533015ebf4e6c
edinvay <evgeni36@yandex.ru>
2025-06-14 17:43:21 +0200

    added a simple regularization step in fractional_matrix_power(A, p) that ensures numerical stability when inverting or taking square rootsof a positive matrix

--- src/lowdin.py
+++ src/lowdin.py
@@ -7,6 +7,8 @@ from scipy.optimize import minimize
 
 def fractional_matrix_power(A, p):
     sigma, U = np.linalg.eigh(A)
+    # Avoid instabilities due to near-zero or negative eigenvalues
+    sigma = np.clip(sigma, 1e-12, None)
     Sm5 = U @ np.diag(sigma**p) @ U.T
     return Sm5
 
